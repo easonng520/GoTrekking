537e8c014a1df335783e2910f36820a41a45ae63
easonng520 <easonng@gmail.com>
2023-06-09 14:15:32 +0800

    test

--- GoTrekking/ViewController.swift
+++ GoTrekking/ViewController.swift
@@ -6,12 +6,41 @@
 //
 
 import UIKit
+import Foundation
+struct Event:Codable{
+    let name: String
+    let date: String
+}
+func decodeAPI(){
+    guard let url = URL(string: "https://b.easonng520.repl.co/api/events") else{return}
+
+    let task = URLSession.shared.dataTask(with: url){
+        data, response, error in
+        
+        let decoder = JSONDecoder()
+
+        if let data = data{
+            do{
+                let tasks = try decoder.decode([Event].self, from: data)
+                tasks.forEach{ i in
+                    print(i.date)
+                }
+            }catch{
+                print(error)
+            }
+        }
+    }
+    task.resume()
+
+}
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        decodeAPI()
     }
 
 
