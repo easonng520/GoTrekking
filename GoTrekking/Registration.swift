8e705a1ca847ec06c37f60d3152fb01222996f5e
easonng520 <easonng@gmail.com>
2023-06-21 14:49:17 +0800

    from recover

---
+++ GoTrekking/Registration.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationViewController.swift
+//  GoTrekking
+//
+//  Created by eazz on 21/6/2023.
+//
+
+import Foundation
+import UIKit
+class Registration: UIViewController {
+    @IBOutlet weak var test: UIButton!
+    @IBAction func ttt(_ sender: Any) {
+        print("AAAA")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
